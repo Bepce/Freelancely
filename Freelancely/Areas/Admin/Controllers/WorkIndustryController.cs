5b82e90c7618902780eb74decb2dde20bc676ead
Georgi Dimitrov <bginfernali@gmail.com>
2024-12-13 18:08:42 +0200

    Page for adding work industries in admin area added.

---
+++ Freelancely/Areas/Admin/Controllers/WorkIndustryController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Freelancely.Web.Areas.Admin.Controllers
+{
+    public class WorkIndustryController : AdminController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+    }
+}
